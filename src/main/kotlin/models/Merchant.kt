13ca28e0f7865558fae5c35bfe662cab2d16c404
kirinico <kirinico@gmail.com>
2025-05-04 10:14:17 +0900

    se agrego merchant y se descomento lo q estaba en store

---
+++ src/main/kotlin/models/Merchant.kt
@@ -0,0 +1,8 @@
+package ar.edu.unsam.phm.models
+
+class Merchant : Identifier {
+  override var id: Long = -1
+  override fun validate() {
+    TODO("Not yet implemented")
+  }
+}
--- src/main/kotlin/models/Store.kt
+++ src/main/kotlin/models/Store.kt
@@ -3,12 +3,12 @@ package ar.edu.unsam.phm.models
 class Store: Locacion {
   override lateinit var name: String
   override var id: Long = -1
+  lateinit var territory: Territory
+  var employees: List<Merchant> = emptyList()
 
   override fun validate() {
     TODO("Not yet implemented")
   }
 
-  lateinit var territory: Territory
-  //var employees: List<Merchand> = emptyList()
 
 }
