310f63825e1373f06375c49934010c2c05e00c22
kirinico <kirinico@gmail.com>
2025-05-05 09:08:27 +0900

    se agrego @Service

--- src/main/kotlin/service/ItemService.kt
+++ src/main/kotlin/service/ItemService.kt
@@ -1,7 +1,9 @@
 package ar.edu.unsam.phm.service
 
 import ar.edu.unsam.phm.dao.ItemRepository
+import org.springframework.stereotype.Service
 
+@Service
 class ItemService(
     val itemRepository: ItemRepository
 ) {
