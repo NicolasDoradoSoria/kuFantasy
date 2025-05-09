2b521ec97db4ec437fcb5f0a34558732ed38bf37
kirinico <kirinico@gmail.com>
2025-05-10 07:24:27 +0900

    se cambio el nombre de la variable username a mail

--- src/main/kotlin/models/User.kt
+++ src/main/kotlin/models/User.kt
@@ -1,7 +1,8 @@
 package ar.edu.unsam.phm.models
 
 class User: Individual(){
-  lateinit var username: String
+  lateinit var mail: String
   lateinit var password: String
 
+  fun verifyPassword(password: String): Boolean = this.password == password
 }
