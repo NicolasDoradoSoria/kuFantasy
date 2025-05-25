1f41b15c81e3de91a07ba88801a2aa63d3808017
kirinico <kirinico@gmail.com>
2025-05-25 10:42:44 +0900

    se agrego el datos al DataInitializer

---
+++ src/main/kotlin/booststrap/DataInitializer.kt
@@ -0,0 +1,190 @@
+package ar.edu.unsam.phm.booststrap
+
+import ar.edu.unsam.phm.dao.ItemRepository
+import ar.edu.unsam.phm.dao.TerritoryRepository
+import ar.edu.unsam.phm.dao.UserRepository
+import ar.edu.unsam.phm.models.InventorySlot
+import ar.edu.unsam.phm.models.Item
+import ar.edu.unsam.phm.models.Territory
+import ar.edu.unsam.phm.models.User
+import ar.edu.unsam.phm.utils.IndividualRole
+import ar.edu.unsam.phm.utils.UserType
+import org.springframework.beans.factory.InitializingBean
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+@Service
+class DataInitializer : InitializingBean {
+
+  @Autowired
+  private lateinit var userRepository: UserRepository
+
+  @Autowired
+  private lateinit var territoryRepository: TerritoryRepository
+
+  @Autowired
+  private lateinit var itemRepository: ItemRepository
+  //***********************
+  // ITEM
+  //***********************
+
+  val espadaMagica = Item().apply {
+    name = "Espada Mágica"
+    description = "Una espada con poder arcano"
+    weight = 10
+    price = 150.0
+    attack = 25
+    image = "espada.jpg"
+  }
+
+  val escudoHierro = Item().apply {
+    name = "Escudo de Hierro"
+    description = "Un escudo resistente"
+    weight = 15
+    price = 100.0
+    defense = 20
+    image = "escudo.jpg"
+  }
+
+
+  //***********************
+  // TERRITORY
+  //***********************
+
+  val bosque = Territory().apply {
+    name = "Bosque Encantado"
+    history = "Un bosque misterioso lleno de magia."
+    image = "bosque.jpg"
+  }
+
+  val torre = Territory().apply {
+    name = "Torre Arcana"
+    history = "Una antigua torre de hechicería prohibida."
+    image = "torre.jpg"
+  }
+
+  val desierto = Territory().apply {
+    name = "Desierto del Olvido"
+    history = "Nadie recuerda qué ocurrió aquí..."
+    image = "desierto.jpg"
+  }
+
+
+
+  //***********************
+  //USERS
+  //***********************
+
+  val valen = User().apply {
+    mail = "valen@example.com"
+    password = "password"
+    name = "Valentino"
+    defense = 20
+    life = 100
+    magic = 50
+    attack = 30
+    speed = 10
+    exp = 150
+    level = 2
+    totalCapacity = 100
+    balance = 250.0
+    currentLocacion = bosque
+    role = IndividualRole.COMMON
+    inventory = listOf(
+      InventorySlot().apply {
+        item = espadaMagica
+        quantity = 1
+      },
+      InventorySlot().apply {
+        item = escudoHierro
+        quantity = 1
+      }
+    )
+  }
+
+  val cris = User().apply {
+    mail = "cris@example.com"
+    password = "password"
+    name = "Cristina"
+    defense = 15
+    life = 120
+    magic = 20
+    attack = 40
+    speed = 15
+    exp = 300
+    level = 3
+    totalCapacity = 120
+    balance = 300.0
+    currentLocacion = torre
+    role = IndividualRole.COMMON
+    type = UserType.PLAYER
+  }
+
+  val mati = User().apply {
+    mail = "mati@example.com"
+    password = "admin123"
+    name = "Matías"
+    defense = 40
+    life = 200
+    magic = 80
+    attack = 70
+    speed = 20
+    exp = 800
+    level = 5
+    totalCapacity = 200
+    balance = 800.0
+    currentLocacion = desierto
+    role = IndividualRole.COMMON
+    type = UserType.PLAYER
+  }
+
+  val juana = User().apply {
+    mail = "juana@example.com"
+    password = "juana123"
+    name = "Juana"
+    defense = 25
+    life = 90
+    magic = 60
+    attack = 20
+    speed = 25
+    exp = 200
+    level = 2
+    totalCapacity = 110
+    balance = 190.0
+    currentLocacion = desierto
+    role = IndividualRole.COMMON
+    type = UserType.PLAYER
+  }
+
+  fun createItems() {
+    itemRepository.apply {
+      create(espadaMagica)
+      create(escudoHierro)
+    }
+    println("items agregados")
+  }
+
+  fun createUsers() {
+    userRepository.apply {
+      create(valen)
+      create(cris)
+      create(juana)
+      create(mati)
+    }
+    println("usuarios agregados")
+  }
+
+  fun createTerritories() {
+    territoryRepository.apply {
+      create(torre)
+      create(desierto)
+      create(bosque)
+    }
+    println("territorios agregados")
+  }
+  override fun afterPropertiesSet() {
+    this.createUsers()
+    this.createTerritories()
+    this.createItems()
+  }
+}
