20544f74f69e4e53acdde30288634d6ef0ec6c59
kirinico <kirinico@gmail.com>
2025-05-27 14:00:35 +0900

    se creo el endpoint de getProfileIndiivudal + service

--- src/main/kotlin/controllers/IndividualController.kt
+++ src/main/kotlin/controllers/IndividualController.kt
@@ -1,16 +1,16 @@
 package ar.edu.unsam.phm.controllers
 
+import ar.edu.unsam.phm.dto.IndividualDTO
 import ar.edu.unsam.phm.service.IndividualService
-import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/individuals")
 @CrossOrigin("*")
-
 class IndividualController(
      val individualService: IndividualService
 ) {
 
+     @GetMapping("/data/{idIndividual}")
+     fun getProfileIndividual(@PathVariable idIndividual: Long): IndividualDTO = IndividualDTO.toDTO(individualService.getProfileIndividual(idIndividual))
 }
--- src/main/kotlin/service/IndividualService.kt
+++ src/main/kotlin/service/IndividualService.kt
@@ -5,5 +5,6 @@ import org.springframework.stereotype.Service
 
 @Service
 class IndividualService(val individualRepository: IndividualRepository) {
+  fun getProfileIndividual(id: Long) = individualRepository.getById(id) ?: throw Exception("No existe un individuo con el id: $id")
 
 }
