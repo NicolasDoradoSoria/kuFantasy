67e938722db3f4eb94238a6df22ac72cc7aebc65
kirinico <kirinico@gmail.com>
2025-05-29 07:06:24 +0900

    se creo un endpoint

--- src/main/kotlin/controllers/TerritoryController.kt
+++ src/main/kotlin/controllers/TerritoryController.kt
@@ -1,8 +1,19 @@
 package ar.edu.unsam.phm.controllers
 
+import ar.edu.unsam.phm.dto.StoreDTO
+import ar.edu.unsam.phm.dto.TerritoryDTO
+import ar.edu.unsam.phm.models.Territory
 import ar.edu.unsam.phm.service.TerritoryService
+import org.springframework.web.bind.annotation.*
 
+@RestController
+@RequestMapping("/territory")
+@CrossOrigin("*")
 class TerritoryController(
   val territoryService: TerritoryService
-) {
+){
+  @GetMapping("/")
+  fun getTerritories(): List<TerritoryDTO> =
+    territoryService.getTerritories().map{TerritoryDTO.from(it) }
+
 }
