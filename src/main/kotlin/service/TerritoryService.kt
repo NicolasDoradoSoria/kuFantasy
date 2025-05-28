1f65718f2bac3af330cfc73b6596da359e65c15c
kirinico <kirinico@gmail.com>
2025-05-29 07:07:32 +0900

    se le agrego a los territorios

--- src/main/kotlin/service/TerritoryService.kt
+++ src/main/kotlin/service/TerritoryService.kt
@@ -1,9 +1,15 @@
 package ar.edu.unsam.phm.service
 
 import ar.edu.unsam.phm.dao.TerritoryRepository
+import ar.edu.unsam.phm.models.Locacion
+import ar.edu.unsam.phm.models.Store
+import org.springframework.stereotype.Service
 
+@Service
 class TerritoryService(
-  val repository: TerritoryRepository
+  val territoryRepository: TerritoryRepository
 ) {
+  fun getTerritories() =  territoryRepository.getAll()
+
 
 }
