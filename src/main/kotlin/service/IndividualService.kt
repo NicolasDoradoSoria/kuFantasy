486bd00b457cd00a5d57ef08e6087f01419351e1
kirinico <kirinico@gmail.com>
2025-06-01 08:17:36 +0900

    se creo un meotodo para hacer update

--- src/main/kotlin/service/IndividualService.kt
+++ src/main/kotlin/service/IndividualService.kt
@@ -1,10 +1,16 @@
 package ar.edu.unsam.phm.service
 
 import ar.edu.unsam.phm.dao.IndividualRepository
+import ar.edu.unsam.phm.models.Individual
 import org.springframework.stereotype.Service
 
 @Service
 class IndividualService(val individualRepository: IndividualRepository) {
-  fun getProfileIndividual(id: Long) = individualRepository.getById(id) ?: throw Exception("No existe un individuo con el id: $id")
+
+
+  fun getById(userId: Long) = individualRepository.getById(userId) ?: throw Exception("No existe un individuo con el id de individual: $userId")
+  fun update(individual: Individual) = individualRepository.update(individual)
+
+
 
 }
