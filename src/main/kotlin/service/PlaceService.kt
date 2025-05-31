5f301bf953b4dc1415df46ca49193ff164a487ca
kirinico <kirinico@gmail.com>
2025-06-01 08:23:15 +0900

    se creo un service PlaceService donde se maneja el inventario de store y houses

---
+++ src/main/kotlin/service/PlaceService.kt
@@ -0,0 +1,27 @@
+package ar.edu.unsam.phm.service
+
+import ar.edu.unsam.phm.dao.PlaceRepository
+import ar.edu.unsam.phm.dto.TakeItemFromPlaceDTO
+import ar.edu.unsam.phm.models.PickUpItemCommand
+import ar.edu.unsam.phm.models.Place
+import org.springframework.stereotype.Component
+
+@Component
+class PlaceService(
+    private val placeRepository: PlaceRepository,
+    private val individualService: IndividualService
+) {
+  fun getById(placeId: Long): Place = placeRepository.getById(placeId) ?: throw Exception("No existe una casa con el id de place: $placeId")
+  fun update(place: Place) = placeRepository.update(place)
+
+  fun pickUpItem(takeItemFromPlaceDTO: TakeItemFromPlaceDTO) {
+    val individual = individualService.getById(takeItemFromPlaceDTO.individualId)
+    val place = this.getById(takeItemFromPlaceDTO.placeId)
+    val item = place.getItemById(takeItemFromPlaceDTO.itemId)
+    PickUpItemCommand(individual, item.item, place).execute()
+
+    individualService.update(individual)
+    this.update(place)
+
+  }
+}
