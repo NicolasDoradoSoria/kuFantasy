6b1fbcaa707b0e21547030f82593da5160cc4de9
kirinico <kirinico@gmail.com>
2025-06-01 08:19:50 +0900

    se le agrego al inventorySlot un companion object

--- src/main/kotlin/models/InventorySlot.kt
+++ src/main/kotlin/models/InventorySlot.kt
@@ -2,5 +2,14 @@ package ar.edu.unsam.phm.models
 
 class InventorySlot {
   lateinit var item: Item
-  var quantity: Int = 0
+  var quantity: Int = 1
+
+  companion object {
+    fun create(item: Item, quantity: Int = 1): InventorySlot {
+      val slot = InventorySlot()
+      slot.item = item
+      slot.quantity = quantity
+      return slot
+    }
+  }
 }
