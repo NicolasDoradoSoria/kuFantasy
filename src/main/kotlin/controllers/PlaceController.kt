5e5320dd3382e7458c03a00c81a8bf76f455d768
kirinico <kirinico@gmail.com>
2025-06-01 08:16:08 +0900

    se crearon controladores

---
+++ src/main/kotlin/controllers/PlaceController.kt
@@ -0,0 +1,17 @@
+package ar.edu.unsam.phm.controllers
+
+import ar.edu.unsam.phm.dto.TakeItemFromPlaceDTO
+
+import ar.edu.unsam.phm.service.PlaceService
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/place")
+@CrossOrigin("*")
+
+class PlaceController(
+  private val placeService: PlaceService
+) {
+  @PatchMapping("/pickup")
+  fun pickUpItemFromPlace(@RequestBody dto: TakeItemFromPlaceDTO) = placeService.pickUpProduct(dto)
+}
