ed0e3ee7cabc90b887b4dfc7b2669c9e4770f53d
kirinico <kirinico@gmail.com>
2025-06-01 08:18:57 +0900

    se crfearon meotodos para manejar el inventario

--- src/main/kotlin/models/Place.kt
+++ src/main/kotlin/models/Place.kt
@@ -10,4 +10,15 @@ abstract class Place : Locacion {
     if (name.isBlank()) throw IllegalArgumentException("Name cannot be blank")
     if (image.isBlank()) throw IllegalArgumentException("Image cannot be blank")
   }
+
+  fun removeItemById(itemId: Long): InventorySlot {
+    val slot = this.getItemById(itemId)
+    inventory = inventory.toMutableList().apply { remove(slot) }
+    return slot
+  }
+
+  fun removeItem(item: Item) : InventorySlot = removeItemById(item.id)
+
+  fun getItemById(itemId: Long): InventorySlot = inventory.find { it.item.id == itemId } ?: throw IllegalArgumentException("Item with id $itemId not found in inventory")
+
 }
