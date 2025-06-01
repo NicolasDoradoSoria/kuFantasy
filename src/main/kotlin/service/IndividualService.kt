04ebcb64c44f39aac824b4fb72f4394f3a98d85e
kirinico <kirinico@gmail.com>
2025-06-01 09:31:01 +0900

    se creo un nuevo metodo en el service IndividualService para comrpar

--- src/main/kotlin/service/IndividualService.kt
+++ src/main/kotlin/service/IndividualService.kt
@@ -1,7 +1,8 @@
 package ar.edu.unsam.phm.service
 
 import ar.edu.unsam.phm.dao.IndividualRepository
-import ar.edu.unsam.phm.models.Individual
+import ar.edu.unsam.phm.dto.BuyItemDTO
+import ar.edu.unsam.phm.models.*
 import org.springframework.stereotype.Service
 
 @Service
@@ -11,6 +12,13 @@ class IndividualService(val individualRepository: IndividualRepository) {
   fun getById(userId: Long) = individualRepository.getById(userId) ?: throw Exception("No existe un individuo con el id de individual: $userId")
   fun update(individual: Individual) = individualRepository.update(individual)
 
+  fun buyItemFrom(buyItemDTO: BuyItemDTO) {
+    val buyer = this.getById(buyItemDTO.individualId)
+    val seller = this.getById(buyItemDTO.sellerId)
 
+    buyer.buyItemFrom(seller, buyItemDTO.itemId)
+    update(buyer)
+    update(seller)
+  }
 
 }
