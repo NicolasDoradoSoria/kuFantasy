4e9b3e0d0ae10ff303c253cd8016af0191027fd2
kirinico <kirinico@gmail.com>
2025-07-05 09:40:33 +0900

    se cambia getAll a findAll TerritoryService

--- src/main/kotlin/service/TerritoryService.kt
+++ src/main/kotlin/service/TerritoryService.kt
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service
 class TerritoryService(
   val territoryRepository: TerritoryRepository
 ) {
-  fun getTerritories() =  territoryRepository.getAll()
+  fun getTerritories() =  territoryRepository.findAll().toList()
 
 
 }
