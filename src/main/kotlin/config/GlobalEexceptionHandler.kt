1f125ca130be25677376a4ba45e63ceb6a8bdb1b
kirinico <kirinico@gmail.com>
2025-07-08 09:25:34 +0900

    se agregaron exceptiones

--- src/main/kotlin/config/GlobalEexceptionHandler.kt
+++ src/main/kotlin/config/GlobalEexceptionHandler.kt
@@ -1,6 +1,6 @@
 package ar.edu.unsam.phm.config
 
-import ar.edu.unsam.phm.utils.exceptions.BusinessException
+import ar.edu.unsam.phm.utils.exceptions.*
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
@@ -9,7 +9,23 @@ import org.springframework.web.bind.annotation.ExceptionHandler
 @ControllerAdvice
 class GlobalEexceptionHandler {
 
-  @ExceptionHandler(Exception::class)
-  fun handleBusinessException(ex: BusinessException) : ResponseEntity<String> =
-    ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.message)
+  @ExceptionHandler(IllegalDataException::class)
+  fun handleIllegalData(e: IllegalDataException): ResponseEntity<SimpleResponse> {
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(SimpleResponse(e.message))
+  }
+
+  @ExceptionHandler(NotFoundException::class)
+  fun handleNotFound(e: NotFoundException): ResponseEntity<SimpleResponse> {
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).body(SimpleResponse(e.message))
+  }
+
+  @ExceptionHandler(InvalidCredentialsException::class)
+  fun handleInvalidCredentials(e: InvalidCredentialsException): ResponseEntity<SimpleResponse> {
+    return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(SimpleResponse(e.message))
+  }
+
+  @ExceptionHandler(InvalidTokenException::class)
+  fun handleInvalidTokenException(e: InvalidTokenException): ResponseEntity<SimpleResponse> {
+    return ResponseEntity.status((HttpStatus.UNAUTHORIZED)).body(SimpleResponse(e.message))
+  }
 }
