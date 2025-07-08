6362f7326eaaea532eb5fb3d254fb5bb937ce22a
kirinico <kirinico@gmail.com>
2025-07-08 09:28:53 +0900

    se refactorizo login para q funcione la seguridad

--- src/main/kotlin/service/LoginService.kt
+++ src/main/kotlin/service/LoginService.kt
@@ -2,19 +2,27 @@ package ar.edu.unsam.phm.service
 
 import ar.edu.unsam.phm.dao.UserRepository
 import ar.edu.unsam.phm.dto.LoginDTO
+import ar.edu.unsam.phm.models.User
+import ar.edu.unsam.phm.security.JwtTokenUtils
 import ar.edu.unsam.phm.utils.exceptions.InvalidCredentialsException
 import org.springframework.stereotype.Component
 
 @Component
 class LoginService(
-  val userRepository: UserRepository
+  private val userService: UserService,
+  private val jwtTokenUtils: JwtTokenUtils
 ) {
-  fun validUser(mail: String) = userRepository.findByMail(mail)   ?: throw InvalidCredentialsException()
-  fun login(loginDTO: LoginDTO) {
-    val user = this.validUser(loginDTO.mail)
-    if(user== null || !user.verifyPassword(loginDTO.password)) {
-      throw InvalidCredentialsException()
-    }
+  fun login(loginDTO: LoginDTO) : String {
+    val user = this.getUser(loginDTO)
+    return jwtTokenUtils.createToken(user.id, user.role.name)!!
   }
 
+  private fun getUser(loginDTO: LoginDTO) : User {
+    val user = userService.getByMail(loginDTO.mail)
+
+    if (!user.verifyPassword(loginDTO.password))
+      throw InvalidCredentialsException()
+
+    return user
+  }
 }
