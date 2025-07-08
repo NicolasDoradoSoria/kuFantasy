3599eb137f76b772ccfc68ae3c308126016e1a71
kirinico <kirinico@gmail.com>
2025-07-08 09:28:26 +0900

    se elimino @CrossOrigin("*") y se refactorizo getProfileIndividual para q funcione la seguridad

--- src/main/kotlin/controllers/IndividualController.kt
+++ src/main/kotlin/controllers/IndividualController.kt
@@ -3,17 +3,22 @@ package ar.edu.unsam.phm.controllers
 import ar.edu.unsam.phm.dto.BuyItemDTO
 import ar.edu.unsam.phm.dto.IndividualDTO
 import ar.edu.unsam.phm.service.IndividualService
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/individuals")
-@CrossOrigin("*")
 class IndividualController(
      val individualService: IndividualService
 ) {
 
-     @GetMapping("/data/{idIndividual}")
-     fun getProfileIndividual(@PathVariable idIndividual: Long): IndividualDTO = IndividualDTO.toDTO(individualService.getById(idIndividual))
+     @ModelAttribute("currentUserId")
+     fun getCurrentUserId(): Long {
+          val usernamePAT = SecurityContextHolder.getContext().authentication
+          return usernamePAT.principal.toString().toLong()
+     }
+     @GetMapping("/data")
+     fun getProfileIndividual(@ModelAttribute("currentUserId") currentUserId: Long): IndividualDTO = IndividualDTO.toDTO(individualService.getById(currentUserId))
 
      @PatchMapping("/buy")
      fun buyItemFromStore(@RequestBody dto: BuyItemDTO) = individualService.buyItemFrom(dto)
