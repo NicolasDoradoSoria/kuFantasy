c564c3a555d49bcc5b9c34e916644e4861bd53d3
kirinico <kirinico@gmail.com>
2025-07-08 09:30:50 +0900

    se cambio para q findByMail devuelva Optional<User> en UserRepository

--- src/main/kotlin/dao/UserRepository.kt
+++ src/main/kotlin/dao/UserRepository.kt
@@ -3,8 +3,9 @@ package ar.edu.unsam.phm.dao
 import ar.edu.unsam.phm.models.User
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
+import java.util.*
 
 @Repository
 interface UserRepository : CrudRepository<User, Long> {
-  fun findByMail(mail: String): User?
+  fun findByMail(mail: String): Optional<User>
 }
