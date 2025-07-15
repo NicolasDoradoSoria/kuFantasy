dc77c93a2f6446ec93714b2b4f18fcd89dcf0ce6
kirinico <kirinico@gmail.com>
2025-07-15 10:09:30 +0900

    se creo un mettodo getById en UserService

--- src/main/kotlin/service/UserService.kt
+++ src/main/kotlin/service/UserService.kt
@@ -1,6 +1,7 @@
 package ar.edu.unsam.phm.service
 
 import ar.edu.unsam.phm.dao.UserRepository
+import ar.edu.unsam.phm.dto.UserInfoDTO
 import ar.edu.unsam.phm.models.User
 import ar.edu.unsam.phm.utils.exceptions.NotFoundException
 import org.springframework.stereotype.Service
@@ -10,6 +11,8 @@ import java.util.*
 class UserService(
   private val userRepository: UserRepository,
 ) {
+  fun getById(userId: Long): User = userRepository.findById(userId).orElseThrow{throw Exception("Usuario no encontrado con ID: $userId")}
+
 
   fun getByMail(mail: String): User =
     userRepository.findByMail(mail).orElseThrow { NotFoundException("Usuario no encontrado $mail")}
@@ -18,5 +21,11 @@ class UserService(
     userRepository.findByMail(mail)
 
   fun save(user : User) = userRepository.save(user)
+
+  fun getCurrentUser(currentUserId: Long): UserInfoDTO {
+    val user = this.getById(currentUserId)
+
+    return UserInfoDTO.from(user)
+  }
 
 }
