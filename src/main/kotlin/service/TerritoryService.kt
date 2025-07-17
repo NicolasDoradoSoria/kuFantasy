099ec9cac07650661b621a55aa215476fb7959f6
kirinico <kirinico@gmail.com>
2025-07-17 13:45:58 +0900

    se agrego un nuevo metodo en el servidor TerritoryService q es el q utiliza el nuevo endpoint q es basicamente para traer 1 territorio completo

--- src/main/kotlin/service/TerritoryService.kt
+++ src/main/kotlin/service/TerritoryService.kt
@@ -2,6 +2,7 @@ package ar.edu.unsam.phm.service
 
 import ar.edu.unsam.phm.dao.TerritoryRepository
 import ar.edu.unsam.phm.models.Territory
+import ar.edu.unsam.phm.utils.exceptions.TerritoryException
 import org.springframework.stereotype.Service
 
 @Service
@@ -10,6 +11,8 @@ class TerritoryService(
 ) {
   fun getTerritories() =  territoryRepository.findAll().toList()
 
+  fun getTerritoryById(id: Long): Territory =
+    territoryRepository.findById(id).orElseThrow { throw TerritoryException("Ha habido un error al recuperar la información del territorio.") }
   fun getRandomTerritory() : Territory {
     if(getTerritories().isEmpty()) throw IllegalStateException("No hay territorios disponibles")
 
