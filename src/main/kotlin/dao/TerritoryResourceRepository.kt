a4892d0429faccfaa726063f436b2538f6d18cb6
kirinico <kirinico@gmail.com>
2025-07-17 13:45:10 +0900

    se creo un repo llamado TerritoryResourceRepository para el modelo TerritoryResource

---
+++ src/main/kotlin/dao/TerritoryResourceRepository.kt
@@ -0,0 +1,8 @@
+package ar.edu.unsam.phm.dao
+
+import ar.edu.unsam.phm.models.TerritoryResource
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface TerritoryResourceRepository : CrudRepository<TerritoryResource, Long>
