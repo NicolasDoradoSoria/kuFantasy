fe7baf98104901cd07f104921fd742e3ec92f6a2
kirinico <kirinico@gmail.com>
2025-07-17 13:46:34 +0900

    nuevo dto basico del modelo Territory llamado TerritorySummaryDTO

---
+++ src/main/kotlin/dto/TerritorySummaryDTO.kt
@@ -0,0 +1,26 @@
+package ar.edu.unsam.phm.dto
+
+import ar.edu.unsam.phm.models.Territory
+import ar.edu.unsam.phm.utils.enums.Difficulty
+import ar.edu.unsam.phm.utils.enums.TerritoryType
+
+data class TerritorySummaryDTO(
+    val id: Long,
+    val name: String,
+    val overview: String,
+    val type: TerritoryType,
+    val level: Int,
+    val difficulty: Difficulty
+) {
+    companion object {
+        fun from(territory: Territory): TerritorySummaryDTO =
+            TerritorySummaryDTO(
+                id = territory.id,
+                name = territory.name,
+                overview = territory.overview,
+                type = territory.type,
+                level = territory.level,
+                difficulty = territory.difficulty
+            )
+    }
+}
