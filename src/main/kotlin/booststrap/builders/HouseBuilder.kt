0cffe92fa2d98d17af8cc76afe640c1c72471247
kirinico <kirinico@gmail.com>
2025-07-22 14:50:39 +0900

    se agrego un nuevo propiedad parentLocatioon al HouseBuilder

--- src/main/kotlin/booststrap/builders/HouseBuilder.kt
+++ src/main/kotlin/booststrap/builders/HouseBuilder.kt
@@ -1,18 +1,16 @@
 package ar.edu.unsam.phm.booststrap.builders
 
-import ar.edu.unsam.phm.models.House
-import ar.edu.unsam.phm.models.InventorySlot
-import ar.edu.unsam.phm.models.Item
-import ar.edu.unsam.phm.models.User
+import ar.edu.unsam.phm.models.*
 
 class HouseBuilder {
   companion object {
-    fun buildMock(name: String, image: String, residents: List<User> = listOf(),  inventoryItems: List<Pair<Item, Int>> = listOf()): House {
+    fun buildMock(name: String, image: String, residents: List<User> = listOf(),  inventoryItems: List<Pair<Item, Int>> = listOf(),  parent: Locacion): House {
       return House().apply {
         this.name = name
         this.image = image
         this.residents = residents.toMutableList()
         this.inventory = inventoryItems.map { InventorySlot.create(it.first, it.second) }.toMutableList()
+        this.parentLocation = parent
       }
     }
   }
