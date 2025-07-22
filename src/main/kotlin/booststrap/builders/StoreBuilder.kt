85a0ec8004769d5dd61b44c367e14009ec4b97c0
kirinico <kirinico@gmail.com>
2025-07-22 14:56:47 +0900

    se agrego una nueva propiedad a StoreBuilder

--- src/main/kotlin/booststrap/builders/StoreBuilder.kt
+++ src/main/kotlin/booststrap/builders/StoreBuilder.kt
@@ -1,18 +1,16 @@
 package ar.edu.unsam.phm.booststrap.builders
 
-import ar.edu.unsam.phm.models.Individual
-import ar.edu.unsam.phm.models.InventorySlot
-import ar.edu.unsam.phm.models.Item
-import ar.edu.unsam.phm.models.Store
+import ar.edu.unsam.phm.models.*
 
 class StoreBuilder {
   companion object {
-    fun buildMock(name: String, seller: Individual, image: String, inventoryItems: List<Pair<Item, Int>> = listOf()): Store {
+    fun buildMock(name: String, seller: Individual, image: String, inventoryItems: List<Pair<Item, Int>> = listOf(), parent: Locacion): Store {
       return Store().apply {
         this.name = name
         this.image = image
         this.seller = seller
         this.inventory = inventoryItems.map { InventorySlot.create(it.first, it.second) }.toMutableList()
+        this.parentLocation = parent
       }
     }
   }
