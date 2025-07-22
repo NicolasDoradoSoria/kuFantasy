06a957746bf9e8e57344ac2f66095e011bce42a2
kirinico <kirinico@gmail.com>
2025-07-22 14:58:53 +0900

    se agrega un nuevo metodo al TerritoryService q sirve para devolver houses stores npcs al territory

--- src/main/kotlin/service/TerritoryService.kt
+++ src/main/kotlin/service/TerritoryService.kt
@@ -1,22 +1,35 @@
 package ar.edu.unsam.phm.service
 
 import ar.edu.unsam.phm.dao.TerritoryRepository
+import ar.edu.unsam.phm.dto.TerritoryDetailDTO
 import ar.edu.unsam.phm.models.Territory
 import ar.edu.unsam.phm.utils.exceptions.TerritoryException
 import org.springframework.stereotype.Service
 
 @Service
 class TerritoryService(
-  val territoryRepository: TerritoryRepository
+  val territoryRepository: TerritoryRepository,
+  val placeService: PlaceService,
+  val individualService: IndividualService
 ) {
   fun getTerritories() =  territoryRepository.findAll().toList()
 
   fun getTerritoryById(id: Long): Territory =
     territoryRepository.findById(id).orElseThrow { throw TerritoryException("Ha habido un error al recuperar la información del territorio.") }
-  fun getRandomTerritory() : Territory {
+  fun getRandomTerritory(): Territory {
     if(getTerritories().isEmpty()) throw IllegalStateException("No hay territorios disponibles")
 
     return getTerritories().random()
   }
+
+  fun getTerritoryDetails(id: Long): TerritoryDetailDTO {
+    val territory = this.getTerritoryById(id)
+    val houses = placeService.getHouses(id)
+    val stores = placeService.getStores(id)
+    val npcs = individualService.getByCurrentLocacionIdAndType(id)
+
+    return TerritoryDetailDTO.from(territory, houses, stores, npcs)
+  }
+
 
 }
