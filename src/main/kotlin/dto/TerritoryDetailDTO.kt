c37a9642012bfb814b32e392cc9fb90cdba368fd
kirinico <kirinico@gmail.com>
2025-07-22 14:58:19 +0900

    se agregan nuevas propiedades a TerritoryDetailDTO

--- src/main/kotlin/dto/TerritoryDetailDTO.kt
+++ src/main/kotlin/dto/TerritoryDetailDTO.kt
@@ -1,12 +1,16 @@
 package ar.edu.unsam.phm.dto
 
+import ar.edu.unsam.phm.models.House
+import ar.edu.unsam.phm.models.Individual
+import ar.edu.unsam.phm.models.Store
 import ar.edu.unsam.phm.models.Territory
 import ar.edu.unsam.phm.utils.enums.Difficulty
 import ar.edu.unsam.phm.utils.enums.TerritoryType
 
-data class TerritoryDetailDTO(
+class TerritoryDetailDTO (
   val id: Long,
   val name: String,
+  val imageUrl: String,
   val overview: String,
   val history: String,
   val level: Int,
@@ -14,13 +18,17 @@ data class TerritoryDetailDTO(
   val type: TerritoryType,
   val info: TerritoryInfoDTO?,
   val resources: List<TerritoryResourceDTO>,
-  val enemies: List<EnemyDTO>
+  val enemies: List<EnemyDTO>,
+  val houses: List<HouseDTO>,
+  val stores: List<StoreDTO>,
+  val npcs: List<NPCDTO>
 ){
   companion object {
-    fun from (territory: Territory): TerritoryDetailDTO =
+    fun from (territory: Territory, houses: List<House>, stores: List<Store>, npcs: List<Individual>): TerritoryDetailDTO =
       TerritoryDetailDTO(
         id= territory.id,
         name = territory.name,
+        imageUrl= territory.image,
         overview = territory.overview,
         history = territory.history,
         level = territory.level,
@@ -28,7 +36,10 @@ data class TerritoryDetailDTO(
         type = territory.type,
         info = territory.info?.let { TerritoryInfoDTO.from(it) },
         resources = territory.resources.map { TerritoryResourceDTO.from(it) },
-        enemies = territory.enemies.map { EnemyDTO.from(it) }
+        enemies = territory.enemies.map { EnemyDTO.from(it) },
+        houses = houses.map { HouseDTO.from(it) },
+        stores = stores.map { StoreDTO.from(it) },
+        npcs = npcs.map { NPCDTO.from(it) }
       )
   }
 }
