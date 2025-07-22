f084e26341d6c2751788e0fc49113ce53ececbda
kirinico <kirinico@gmail.com>
2025-07-22 14:56:28 +0900

    se agrego en RaceService un metodo para devolver una raza en especifico

--- src/main/kotlin/service/RaceService.kt
+++ src/main/kotlin/service/RaceService.kt
@@ -1,10 +1,13 @@
 package ar.edu.unsam.phm.service
 
 import ar.edu.unsam.phm.dao.RaceRepository
+import ar.edu.unsam.phm.models.Race
 import org.springframework.stereotype.Component
 
 @Component
 class RaceService(private val raceRepository: RaceRepository) {
+
+  fun getById(raceId: Long): Race = raceRepository.findById(raceId).orElseThrow{ throw Exception("No existe una raza con el id: $raceId") }
 
   fun getRaces() = raceRepository.findAll().toList()
 }
