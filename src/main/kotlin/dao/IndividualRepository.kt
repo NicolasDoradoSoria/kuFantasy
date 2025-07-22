eb2dfaab1e9e4dba01d88bfa0dd04fa2587923a2
kirinico <kirinico@gmail.com>
2025-07-22 14:53:37 +0900

    se agrego una nueva query a IndividualRepository y en IndividualService se llama a esa query con un nuevo metodo. a parte se agrega un nuevo meotodo transaccional q es para asignar la raza

--- src/main/kotlin/dao/IndividualRepository.kt
+++ src/main/kotlin/dao/IndividualRepository.kt
@@ -1,8 +1,12 @@
 package ar.edu.unsam.phm.dao
 
 import ar.edu.unsam.phm.models.Individual
+import ar.edu.unsam.phm.utils.UserType
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface IndividualRepository : CrudRepository<Individual, Long>
+interface IndividualRepository : CrudRepository<Individual, Long>{
+
+  fun findByCurrentLocacionIdAndType(territoryId : Long, type: UserType) : List<Individual>
+}
--- src/main/kotlin/service/IndividualService.kt
+++ src/main/kotlin/service/IndividualService.kt
@@ -3,13 +3,15 @@ package ar.edu.unsam.phm.service
 import ar.edu.unsam.phm.dao.IndividualRepository
 import ar.edu.unsam.phm.dto.BuyItemDTO
 import ar.edu.unsam.phm.models.*
+import ar.edu.unsam.phm.utils.UserType
+import org.apache.coyote.BadRequestException
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
-class IndividualService(val individualRepository: IndividualRepository) {
+class IndividualService(val individualRepository: IndividualRepository, val raceService: RaceService) {
 
-
-  fun getById(userId: Long) = individualRepository.findById(userId).orElseThrow{ throw Exception("No existe un individuo con el id de individual: $userId") }
+  fun getById(userId: Long): Individual = individualRepository.findById(userId).orElseThrow{ throw Exception("No existe un individuo con el id de individual: $userId") }
 
   fun update(individual: Individual) = individualRepository.save(individual)
 
@@ -21,5 +23,18 @@ class IndividualService(val individualRepository: IndividualRepository) {
     update(buyer)
     update(seller)
   }
+
+  @Transactional
+  fun assignRaceToIndividual(idRace: Long, currentUserId: Long){
+    val user = this.getById(currentUserId)
+
+    if(user.race != null) throw BadRequestException("El personaje ya tiene una raza asignada")
+
+    val race = raceService.getById(idRace)
+
+    user.race =race
+  }
+
+  fun getByCurrentLocacionIdAndType(territoryId: Long) = individualRepository.findByCurrentLocacionIdAndType(territoryId, UserType.NPC)
 
 }
