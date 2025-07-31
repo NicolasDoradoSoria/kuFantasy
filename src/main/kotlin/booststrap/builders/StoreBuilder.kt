010acd55fcd14c91cd485b30299984608901126e
kirinico <kirinico@gmail.com>
2025-08-01 07:55:51 +0900

    producto de la nueva funcion en Store.kt se agrego una viriable type a StoreBuilder

--- src/main/kotlin/booststrap/builders/StoreBuilder.kt
+++ src/main/kotlin/booststrap/builders/StoreBuilder.kt
@@ -1,16 +1,18 @@
 package ar.edu.unsam.phm.booststrap.builders
 
 import ar.edu.unsam.phm.models.*
+import ar.edu.unsam.phm.utils.enums.StoreType
 
 class StoreBuilder {
   companion object {
-    fun buildMock(name: String, seller: Individual, image: String, inventoryItems: List<Pair<Item, Int>> = listOf(), parent: Locacion): Store {
+    fun buildMock(name: String, seller: Individual, image: String, inventoryItems: List<Pair<Item, Int>> = listOf(), parent: Locacion, type: StoreType = StoreType.GENERAL_STORE ): Store {
       return Store().apply {
         this.name = name
         this.image = image
         this.seller = seller
         this.inventory = inventoryItems.map { InventorySlot.create(it.first, it.second) }.toMutableList()
         this.parentLocation = parent
+        this.type = type
       }
     }
   }
