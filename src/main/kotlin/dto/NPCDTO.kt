96a90cc797020f914b21ba1524d6a185404fc2d8
kirinico <kirinico@gmail.com>
2025-08-01 07:54:37 +0900

    se agrego una variable nueva a NPCDTO.kt

--- src/main/kotlin/dto/NPCDTO.kt
+++ src/main/kotlin/dto/NPCDTO.kt
@@ -6,14 +6,16 @@ import ar.edu.unsam.phm.utils.IndividualRole
 data class NPCDTO (
   val id: Long,
   val name: String,
-  val role: IndividualRole
+  val role: IndividualRole,
+  val imageUrl: String
 ){
   companion object {
     fun from(npc: Individual) : NPCDTO {
       return NPCDTO(
         id  =npc.id,
         name = npc.name,
-        role = npc.role
+        role = npc.role,
+        imageUrl = npc.imageUrl
       )
     }
   }
