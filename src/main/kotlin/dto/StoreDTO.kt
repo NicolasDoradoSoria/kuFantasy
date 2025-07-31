d8c6c41f94c05232c8fd4d1fc903594d4a5dfadc
kirinico <kirinico@gmail.com>
2025-08-01 07:56:01 +0900

    producto de la nueva funcion en Store.kt se agrego una viriable type a StoreBuilder

--- src/main/kotlin/dto/StoreDTO.kt
+++ src/main/kotlin/dto/StoreDTO.kt
@@ -2,13 +2,15 @@ package ar.edu.unsam.phm.dto
 
 import ar.edu.unsam.phm.models.Locacion
 import ar.edu.unsam.phm.models.Store
+import ar.edu.unsam.phm.utils.enums.StoreType
 
 
 data class StoreDTO(
     val id: Long,
     val name: String,
     val image: String,
-    val inventory: List<InventorySlotDTO>
+    val inventory: List<InventorySlotDTO>,
+    val type: StoreType
 ) {
     companion object {
         fun from(store: Store): StoreDTO {
@@ -16,7 +18,8 @@ data class StoreDTO(
                 id = store.id,
                 name = store.name,
                 image = store.image,
-                inventory = store.inventory.map { InventorySlotDTO.from(it) }
+                inventory = store.inventory.map { InventorySlotDTO.from(it) },
+                type = store.type
             )
         }
     }
