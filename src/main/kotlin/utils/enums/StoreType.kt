a1e696a574c8a5464b7a7614ee7243085a49aca6
kirinico <kirinico@gmail.com>
2025-08-01 07:56:25 +0900

    se agrego un lista de tiendas en StoreType.kt

---
+++ src/main/kotlin/utils/enums/StoreType.kt
@@ -0,0 +1,25 @@
+package ar.edu.unsam.phm.utils.enums
+
+enum class StoreType {
+  GENERAL_STORE,      // Tienda general
+  BLACKSMITH,         // Herrero
+  ARMOR_SHOP,         // Armaduras
+  WEAPON_SHOP,        // Armas
+  MAGIC_SHOP,         // Objetos mágicos y hechizos
+  ALCHEMY_SHOP,       // Pociones y alquimia
+  INN,                // Posada
+  TAVERN,             // Taberna
+  STABLE,             // Caballos y monturas
+  FISHMONGER,         // Pescadería
+  BAKERY,             // Panadería
+  HERBALIST,          // Hierbas y medicina natural
+  CLOTHING_STORE,     // Ropa
+  JEWELRY_SHOP,       // Joyas
+  PET_SHOP,           // Mascotas o criaturas
+  BOOKSTORE,          // Libros y pergaminos
+  CURIO_SHOP,         // Objetos curiosos o raros
+  CARTOGRAPHER,       // Mapas y navegación
+  HUNTER_SHACK,       // Caza y trampas
+  FLETCHER,           // Flechas y arcos
+  FARMERS_MARKET      // Comida fresca y productos rurales
+}
