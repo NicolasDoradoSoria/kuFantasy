f6b0ce88b277900a6b3e5b695f93e4d643ccb62a
kirinico <kirinico@gmail.com>
2025-08-01 07:57:21 +0900

    se modifico la clase TerritoryDetailDTO

--- src/main/kotlin/dto/TerritoryDetailDTO.kt
+++ src/main/kotlin/dto/TerritoryDetailDTO.kt
@@ -4,19 +4,12 @@ import ar.edu.unsam.phm.models.House
 import ar.edu.unsam.phm.models.Individual
 import ar.edu.unsam.phm.models.Store
 import ar.edu.unsam.phm.models.Territory
-import ar.edu.unsam.phm.utils.enums.Difficulty
-import ar.edu.unsam.phm.utils.enums.TerritoryType
+
 
 class TerritoryDetailDTO (
   val id: Long,
   val name: String,
   val imageUrl: String,
-  val overview: String,
-  val history: String,
-  val level: Int,
-  val difficulty: Difficulty,
-  val type: TerritoryType,
-  val info: TerritoryInfoDTO?,
   val resources: List<TerritoryResourceDTO>,
   val enemies: List<EnemyDTO>,
   val houses: List<HouseDTO>,
@@ -29,12 +22,6 @@ class TerritoryDetailDTO (
         id= territory.id,
         name = territory.name,
         imageUrl= territory.image,
-        overview = territory.overview,
-        history = territory.history,
-        level = territory.level,
-        difficulty = territory.difficulty,
-        type = territory.type,
-        info = territory.info?.let { TerritoryInfoDTO.from(it) },
         resources = territory.resources.map { TerritoryResourceDTO.from(it) },
         enemies = territory.enemies.map { EnemyDTO.from(it) },
         houses = houses.map { HouseDTO.from(it) },
